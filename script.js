02e3d8f61f852cda039d8822f2cf6bf3f05a2ed8
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2024-11-22 13:30:42 -0800

    Completed a component and then used $emit onClick event to increase on 1 btn and decrease 1 on other btn

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-const app = Vue.createApp({});
+// const app = Vue.createApp({});
 
-app.component("my-first-component", {
+// app.component("my-first-component", {
+// 	data() {
+// 		return {
+// 			propertyName: 0,
+// 		};
+// 	},
+// 	template: `<button v-on:click='propertyName++'>
+// 			Clicked {{ propertyName }} times
+// 		</button>`,
+// });
+
+// app.mount("#my-first-div");
+
+// const app = Vue.createApp({});
+
+// app.component("header-component", {
+// 	props: ["title"],
+// 	template: `<h2>{{title}}</h2>`,
+// });
+
+// app.mount("#component-props-div");
+
+const app = Vue.createApp({
 	data() {
 		return {
-			propertyName: 0,
+			headers: [
+				{ id: 1, title: "My First Header" },
+				{ id: 2, title: "My Second Header" },
+				{ id: 3, title: "My Third Header" },
+			],
+			headerFontSize: 1,
 		};
 	},
-	template: `<button v-on:click='propertyName++'>
-			Clicked {{ propertyName }} times
-		</button>`,
 });
 
-app.mount("#my-first-div");
+app.component("header-component", {
+	props: ["title"],
+	template: `<div class=header-component>
+		<h2>{{title}}</h2>
+		<button @click="$emit('increase-text-size')">
+			Increase Text Size
+		</button>
+		<button @click="$emit('decrease-text-size')">
+			Decrease Text Size
+		</button>
+	</div>`,
+});
+
+app.mount("#emit-events-div");
