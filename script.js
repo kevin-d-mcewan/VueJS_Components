73813367c433fe85f8ab857fbe56144055accc20
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2024-11-22 14:17:33 -0800

    added a dynamic component

--- script.js
+++ script.js
@@ -1,51 +1,28 @@
-// const app = Vue.createApp({});
-
-// app.component("my-first-component", {
-// 	data() {
-// 		return {
-// 			propertyName: 0,
-// 		};
-// 	},
-// 	template: `<button v-on:click='propertyName++'>
-// 			Clicked {{ propertyName }} times
-// 		</button>`,
-// });
-
-// app.mount("#my-first-div");
-
-// const app = Vue.createApp({});
-
-// app.component("header-component", {
-// 	props: ["title"],
-// 	template: `<h2>{{title}}</h2>`,
-// });
-
-// app.mount("#component-props-div");
-
 const app = Vue.createApp({
 	data() {
 		return {
-			headers: [
-				{ id: 1, title: "My First Header" },
-				{ id: 2, title: "My Second Header" },
-				{ id: 3, title: "My Third Header" },
-			],
-			headerFontSize: 1,
+			currentTab: "Homepage",
+			tabs: ["Homepage", "Products", "Contact"],
 		};
+	},
+	computed: {
+		currentTabComponent() {
+			return "tab-" + this.currentTab.toLowerCase();
+		},
 	},
 });
 
-app.component("header-component", {
-	props: ["title"],
-	template: `<div class=header-component>
-		<h2>{{title}}</h2>
-		<button @click="$emit('increase-text-size')">
-			Increase Text Size
-		</button>
-		<button @click="$emit('decrease-text-size')">
-			Decrease Text Size
-		</button>
-	</div>`,
+app.component("tab-homepage", {
+	template: `<div
+							class="navigation">homepage information</div>`,
 });
 
-app.mount("#emit-events-div");
+app.component("tab-products", {
+	template: `<div	class="navigation">product information</div>`,
+});
+
+app.component("tab-contact", {
+	template: `<div class="navigation">contact information</div>`,
+});
+
+app.mount("#navigation-div");
